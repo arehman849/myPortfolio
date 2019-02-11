3c9887b8cdfbe7e03f4b0a38e674daa636f3041e
anees rahiman <anees.syed@nagra.com>
2019-02-11 09:10:22 +0530

    adding app.js file as entry point
    adding app.js file as entry point to the website.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
-import Header from "./components/header";
+import App from "./app";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import "jquery/dist/jquery.js";
 import "popper.js/dist/popper.js";
 
-ReactDOM.render(<Header />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
